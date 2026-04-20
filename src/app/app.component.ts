7255f9fa66708bfb20017205ad8add9ae6deeb48
cesargusa <c.gutierrez@spherag.com>
2026-04-20 11:53:02 +0200

    min changes and add footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { MainBannerComponent } from './main-banner/main-banner.component';
 import { PostsPageComponent } from './posts-page/posts-page.component';
 import { CardsPageComponent } from './cards-page/cards-page.component';
+import { FooterPageComponent } from './footer-page/footer-page.component';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [MainBannerComponent, PostsPageComponent,CardsPageComponent],
+  imports: [MainBannerComponent, PostsPageComponent,CardsPageComponent,FooterPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
